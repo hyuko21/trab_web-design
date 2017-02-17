82ec96eebe0bba783522414472fe142336516fc9
hyuko21 <victorwdr@gmail.com>
2017-02-17 20:21:18 -0300

    att6

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+window.onload = function() {
+	var path = location.pathname;
+	var page = path.split("/").pop();
+	
+	if (page == 'aluno.html' || page == 'professor.html'){
+		document.body.style.background = 'linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .9)), url(imagem/harvard_alunos.jpg) no-repeat';
+		document.body.style.backgroundPosition = 'center';
+		document.body.style.backgroundSize = 'cover';
+		document.body.style.backgroundAttachment = 'fixed';
+	} else if (page == 'pais.html' || page == 'secretaria.html'){
+		document.body.style.background = 'linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .9)), url(imagem/harvard_serious.jpg) no-repeat';
+		document.body.style.backgroundPosition = 'center';
+		document.body.style.backgroundSize = 'cover';
+		document.body.style.backgroundAttachment = 'fixed';
+	}
+}
+
+//Botão para voltar à parte superior da página
 $(document).ready(function(){
 
 	//Check to see if the window is top if not then display button
@@ -16,6 +34,7 @@ $(document).ready(function(){
 	});
 });
 
+//Scroll da página clicando nos links(História/Contato)
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
